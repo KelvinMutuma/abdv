5e5042bfcd11302dcd0ecffb68fbfcb62b2f25bb
KelvinMutuma <muthuri.kelmut@gmail.com>
2014-06-11 12:39:31 +0300

    Configured app tokens

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.2.1'
 
 set :application, 'nse_data'
 set :repo_url, 'git@github.com:KelvinMutuma/abdv.git'
-
+set :branch, "master"
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
@@ -11,7 +11,7 @@ set :repo_url, 'git@github.com:KelvinMutuma/abdv.git'
 # set :deploy_to, '/var/www/my_app'
 # set :deploy_to, '/usr/share/nginx/www'
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty
